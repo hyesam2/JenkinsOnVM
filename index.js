171cacc53b39403ca33942646d1b1967290f1b5c
Hyein <hyesamsam@gmail.com>
2021-07-23 02:35:53 +0900

    4th

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I Like Azure!');
+    res.send('I Like azure!');
 });
 
 app.get('/start', function(req, res){
