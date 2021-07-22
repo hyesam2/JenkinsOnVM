23dec6e821d7c6c6f11f464ab7cbc0f27c15789f
Hyein <hyesamsam@gmail.com>
2021-07-23 02:53:03 +0900

    3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('sibal sibal  why?!');
+    res.send('sibal........');
 });
 
 app.get('/start', function(req, res){
