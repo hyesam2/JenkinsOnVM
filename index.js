f32824ddf79f9c8410a7557a258cc056e6a426ba
Hyein <hyesamsam@gmail.com>
2021-07-23 02:58:23 +0900

    3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I Like Jenkins!');
+    res.send('What the,,, kins!');
 });
 
 app.get('/start', function(req, res){
