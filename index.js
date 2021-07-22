4d555d4d5ffc5fb1b1babfcfabea0dcfc9eac941
Hyein <hyesamsam@gmail.com>
2021-07-23 02:05:48 +0900

    First

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I Love Hyein!');
+    res.send('Hello World!!!!!!');
 });
 
 app.get('/start', function(req, res){
