e9e544a2458f420e140ecd0e36e5ebb1bc28bd7a
hyesam2 <hyesamsam@gmail.com>
2022-08-01 12:47:48 +0900

    3rd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I Don't Like Jenkins');
+    res.send('I Like Myself');
 });
 
 app.get('/start', function(req, res){
