264276d5044ac8dbe3e643cd853c45c27225e39c
hyesam2 <hyesamsam@gmail.com>
2022-08-01 12:52:22 +0900

    6th

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I Like Coding');
+    res.send('I Like SNSD');
 });
 
 app.get('/start', function(req, res){
